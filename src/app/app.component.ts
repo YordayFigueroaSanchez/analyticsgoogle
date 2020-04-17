cea70f03ab2f878533385d4f095e164b29e2363d
yfigueroa <yfsanchez@gmail.com>
2020-04-17 17:41:23 -0300

    inicio
    inicio

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter} from 'rxjs/operators';
+
+declare var gtag;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'analyticsgoogle';
+
+  constructor(
+    private router: Router
+  ){
+    const navEndEvents$ = this.router.events
+.pipe(
+  filter(event => event instanceof NavigationEnd)
+);
+
+navEndEvents$.subscribe((event: NavigationEnd) => {
+  gtag('config', 'UA-163877115-1', {
+    'page_path': event.urlAfterRedirects
+  });
+});
+  }
+}
